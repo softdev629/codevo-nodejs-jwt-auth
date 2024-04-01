595dcb31522b7020cc88b0152eb99f910feb8f8d
Eugine <121113067+softdev629@users.noreply.github.com>
2024-04-01 13:59:59 -0700

    refresh_token

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -35,12 +35,14 @@ export const findUser = async (
 // Sign Token
 export const signToken = async (user: DocumentType<User>) => {
   // Sign the access token
-  const access_token = signJwt(
-    { sub: user._id },
-    {
-      expiresIn: `${config.get<number>("accessTokenExpiresIn")}m`,
-    }
-  );
+  const access_token = signJwt({ sub: user._id }, "accessTokenPrivateKey", {
+    expiresIn: `${config.get<number>("accessTokenExpiresIn")}m`,
+  });
+
+  // Sign the refresh token
+  const refresh_token = signJwt({ sub: user._id }, "refreshTokenPrivateKey", {
+    expiresIn: `${config.get<number>("refreshTokenExpiresIn")}m`,
+  });
 
   // Create a Session
   redisClient.set(user._id.toString(), JSON.stringify(user), {
@@ -48,5 +50,5 @@ export const signToken = async (user: DocumentType<User>) => {
   });
 
   // Return access token
-  return { accessToken: access_token };
+  return { access_token, refresh_token };
 };
